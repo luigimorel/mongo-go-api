eb5b01073d5ed58d19e6e2e2b69c447c69d5460c
Luigi Morel <morelmiles@gmail.com>
2023-02-09 23:11:19 +0300

    add auth and user routes

---
+++ routes/user.routes.go
@@ -0,0 +1,24 @@
+package routes
+
+import (
+	"mongo-auth/controllers"
+	"mongo-auth/middleware"
+	"mongo-auth/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+type UserRouteController struct {
+	userController controllers.UserController
+}
+
+func NewRouteUserController(userController controllers.UserController) UserRouteController {
+	return UserRouteController{userController}
+}
+
+func (uc *UserRouteController) UserRoute(rg *gin.RouterGroup, userService services.UserService) {
+
+	router := rg.Group("users")
+	router.Use(middleware.DeserializeUser(userService))
+	router.GET("/profile", uc.userController.GetProfile)
+}
